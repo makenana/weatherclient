5293365d1508a1ccd807c37409bdd2b94a22667b
Makenana <makenabambrick17@gmail.com>
2024-02-16 20:52:53 -0800

    02/16/2024

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Component({
+  selector: 'app-hello',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello.component.html',
+  styleUrl: './hello.component.css'
+})
+export class HelloComponent implements OnInit {
+  public forecasts: WeatherForecast[] = [];
+  public baseUrl: string = "http://localhost:5130";
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getForecasts();
+  }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>(this.baseUrl + '/WeatherForecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+}
